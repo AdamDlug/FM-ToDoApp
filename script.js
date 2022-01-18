5fdaa4a84d9027051f0046c8e5d3fbb15a916e85
AdamDlug <89133957+AdamDlug@users.noreply.github.com>
2022-01-18 08:10:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,19 +6,88 @@ const themeSwitch = document.getElementById('theme-switch')
 const newItemText = document.getElementById('NewItemInput')
 
 
-
-//////////////  dodanie nowego elementu listy  /////////////////
-for (let i = -1; i < todoList.length; i++) {
-    newItemAccept.addEventListener('click', function(){
-        document.getElementById('Todo-list').innerHTML += 
-        `<div class="new-item">
-            <div class="div-checkbox"></div>
-            <div>${newItemText.value}</div>         
-        </div>
-        `
-        newItemText.value=""
-    })
+///////////// zlicza elementy które istnieją w liscie
+const licznikElementow = () => {
+    document.getElementById('itemsLeft-span').innerHTML = document.querySelectorAll('.new-item').length;
 }
+
+
+
+///////////// dodanie nowego elementu //////////////////
+
+newItemAccept.addEventListener('click', function(){
+
+    document.getElementById('Todo-list').innerHTML += 
+    `<div class="new-item">
+        <div class="new-item-content">
+            <div class="div-checkbox"></div>
+            <div>${newItemText.value}</div>
+        </div>
+        <div><img src="./images/icon-cross.svg" class="DeleteElement"></div> 
+    </div>
+    `
+    newItemText.value="";
+    licznikElementow();
+    ItemDelete(); 
+})
+
+
+///////////// przycisk usun element //////////////////
+/*
+    trzeba poprawić usuwanie bo przy zmiennej kolejności usuwania wywala błąd
+*/
+const ItemDelete = () => {
+    const testusuwanie = document.getElementsByClassName('new-item')
+    const przyciskUsunEl = document.getElementsByClassName('DeleteElement')
+    for (let i = 0; i < przyciskUsunEl.length; i++) {
+        przyciskUsunEl[i].addEventListener('click', function () {
+            
+            testusuwanie[i].parentNode.removeChild(testusuwanie[i])
+            i--
+            console.log(document.querySelectorAll('.new-item'))
+            licznikElementow();
+        })  
+    }
+}
+
+///////////// drag drop //////////////////
+/*
+    
+*/
+const DragDrop = () => {
+    const testDragDrop = document.getElementsByClassName('new-item')
+    for (let i = 0; i < testDragDrop.length; i++) {
+        testDragDrop[i].addEventListener('', function () {
+            
+            testusuwanie[i].parentNode.removeChild(testusuwanie[i])
+            i--
+            console.log(document.querySelectorAll('.new-item'))
+            licznikElementow();
+        })  
+    }
+}
+
+///////////// przycisk potwierdzajacy wykonanie zadania //////////////////
+
+const taskDone = () => {
+    for (let i = 0; i < testMouseOver.length; i++) {
+        testMouseOver[i].addEventListener('click', function () {
+            
+            testusuwanie[i].parentNode.removeChild(testusuwanie[i])
+            console.log(i)
+            console.log(document.querySelectorAll('.new-item'))
+            licznikElementow();
+        })  
+    }
+}
+
+
+
+
+
+
+
+
 
 //////////// zmiana motywu ////////////
 let count = 1
@@ -44,3 +113,25 @@ for (let i = -1; i < todoList.length; i++) {
 }
 
 ///////////////  zmiana na ukończone /////////////// 
+
+
+
+
+// ///////////// przycisk usun element //////////////////
+// document.getElementById('DeleteElement').addEventListener('click', function() {
+//     for (let i = 0; i < 5; i++) {
+//         console.log(`pozycja ${i}`)
+//     }
+// })
+
+// let testArray = new Array();
+// testArray[0] = "element 0"
+// testArray[1] = "element 1"
+// testArray[2] = "element 2"
+// testArray[3] = "element 3"
+
+// testArray.forEach(element => {
+//     console.log(element)
+// });
+
+// testArray.splice(2,1)
